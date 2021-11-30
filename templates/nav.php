1a6bd751210acbb54e3ca3a3f590fcbed30ab0ca
Marina Rose <maffia.marina@gmail.com>
2021-11-30 09:28:47 -0500

    updated links with directory name

--- templates/nav.php
+++ templates/nav.php
@@ -1,5 +1,5 @@
 <div class="navbar">
-    <div class="item"><a href="../">Home</a></div>
-    <div class="item"><a href="../register/">Register</a></div>
-    <div class="item"><a href="../login/">Login</a></div>
+    <div class="item"><a href="/login-demo">Home</a></div>
+    <div class="item"><a href="/login-demo/register/">Register</a></div>
+    <div class="item"><a href="/login-demo/login/">Login</a></div>
 </div>
